d3d677120205c75b751e1cbf5164736d4347daf4
Celso Aquino <cvaquino03@outlook.com>
2024-10-02 08:19:51 +0800

    Updated CORS 2

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(bodyParser.json({ limit: '10mb' })); // To handle large base64 images
